bb25fcac3ade284741d4e65f2f8e836f463e92af
evertoont <evertontbr@gmail.com>
2020-05-07 21:39:17 -0300

    Adiciona Projeto Conversor de Imagem em P&B

---
+++ conversor_de_imagem_pb.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+def converter(caminho):
+
+    try:
+        with Image.open(caminho) as abrir_imagee:
+
+            converter_pbb = abrir_imagee.convert('L')
+
+            converter_pbb.show()
+
+            print('Imagem convertida com sucesso!')
+    except:
+        print("Erro ao converter imagem, verifique o caminho indicado!")
+
+
+print("""
+======================================================
+Digite abaixo o caminho de onde se encontra a imagem.
+
+Exemplo: C:\\Users\\NOME_USUARIO\\imagens\\foto.png'
+======================================================
+""")
+
+
+caminho_imagem = input("Digite o caminho da imagem: ").strip()
+
+converter(caminho_imagem)
