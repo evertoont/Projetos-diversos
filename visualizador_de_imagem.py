017fb9b1dcaec22832b94a6f2d94bcf9e8598cc7
evertoont <evertontbr@gmail.com>
2020-06-18 21:24:49 -0300

    Adiciona projeto Visualizador de imagem

---
+++ visualizador_de_imagem.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+
+def carregarImagem(diretorio):
+    with Image.open(diretorio) as Imagem:
+        renderizar = ImageTk.PhotoImage(Imagem)
+        img = tk.Label(image=renderizar)
+        img.image = renderizar
+        img.place(x=0, y=0)
+        return Imagem.size
+
+
+def main():
+    print('\nDigite o diretório da imagem abaixo (Siga o exemplo)')
+    print("Exemplo -> C:\\Users\\YourName\\Pictures\\avatar.png: ")
+
+    diretorio = input("URL: ")
+    root = tk.Tk()
+    altura, largura = carregarImagem(diretorio)
+    root.wm_title('Visualizador de imagem')
+    root.geometry(f"{altura}x{largura}")
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
